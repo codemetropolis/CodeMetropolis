036aa37247d83c047db0bf92aa6f8f351f13db8b
Hanziness <hanziness@windowslive.com>
2018-04-20 16:41:41 +0200

    Swapped arguments to match JUnit 'expected' and 'actual' parameters in ConverterExecutorArgs and MappingExecutorArgs tests

--- sources/codemetropolis-toolchain-converter/src/test/java/codemetropolis/toolchain/converter/ConverterExecutorArgsTest.java
+++ sources/codemetropolis-toolchain-converter/src/test/java/codemetropolis/toolchain/converter/ConverterExecutorArgsTest.java
@@ -6,25 +6,25 @@ import codemetropolis.toolchain.converter.control.ConverterType;
 import junit.framework.TestCase;
 
 public class ConverterExecutorArgsTest extends TestCase {
-	ConverterExecutorArgs result;
+	ConverterExecutorArgs testObj;
 	
 	public void setup () {
-		result = null;
+		testObj = null;
 	}
 	
 	public void testConverterExecutorArgsConstructorNoParams() {
-		result = new ConverterExecutorArgs(ConverterType.SOURCEMETER, "testSource", "testOutput");
-		assertEquals(result.getOutputFile(), "testOutput");
-		assertEquals(result.getSource(), "testSource");
-		assertEquals(result.getType(), ConverterType.SOURCEMETER);
+		testObj = new ConverterExecutorArgs(ConverterType.SOURCEMETER, "testSource", "testOutput");
+		assertEquals("testOutput", testObj.getOutputFile());
+		assertEquals("testSource", testObj.getSource());
+		assertEquals(ConverterType.SOURCEMETER, testObj.getType());
 	}
 	
 	public void testConverterExecutorArgsConstructor() {
-		result = new ConverterExecutorArgs(ConverterType.SONARQUBE, "testSource", "testOutput", new HashMap<String, String>());
-		assertEquals(result.getOutputFile(), "testOutput");
-		assertEquals(result.getSource(), "testSource");
-		assertEquals(result.getType(), ConverterType.SONARQUBE);
-		assertNotNull(result.getParams());
-		assertTrue(result.getParams().isEmpty());
+		testObj = new ConverterExecutorArgs(ConverterType.SONARQUBE, "testSource", "testOutput", new HashMap<String, String>());
+		assertEquals("testOutput", testObj.getOutputFile());
+		assertEquals("testSource", testObj.getSource());
+		assertEquals(ConverterType.SONARQUBE, testObj.getType());
+		assertNotNull(testObj.getParams());
+		assertTrue(testObj.getParams().isEmpty());
 	}
 }
--- sources/codemetropolis-toolchain-mapping/src/test/java/codemetropolis/toolchain/mapping/MappingExecutorArgsTest.java
+++ sources/codemetropolis-toolchain-mapping/src/test/java/codemetropolis/toolchain/mapping/MappingExecutorArgsTest.java
@@ -4,19 +4,19 @@ import junit.framework.TestCase;
 
 public class MappingExecutorArgsTest extends TestCase {
 
-	MappingExecutorArgs result;
+	MappingExecutorArgs testObj;
 	
 	public void setup() {
-		result = null;
+		testObj = null;
 	}
 	
 	public void testMappingExecutorArgs() {
-		result = new MappingExecutorArgs("testCDF", "testOutput", "testMapping", 1.0, true);
-		assertEquals(result.getCdfFile(), "testCDF");
-		assertEquals(result.getOutputFile(), "testOutput");
-		assertEquals(result.getMappingFile(), "testMapping");
-		assertEquals(result.getScale(), 1.0);
-		assertEquals(result.isHierarchyValidationEnabled(), true);
+		testObj = new MappingExecutorArgs("testCDF", "testOutput", "testMapping", 1.0, true);
+		assertEquals("testCDF", testObj.getCdfFile());
+		assertEquals("testOutput", testObj.getOutputFile());
+		assertEquals("testMapping", testObj.getMappingFile());
+		assertEquals(1.0, testObj.getScale());
+		assertEquals(true, testObj.isHierarchyValidationEnabled());
 	}
 	
 }
