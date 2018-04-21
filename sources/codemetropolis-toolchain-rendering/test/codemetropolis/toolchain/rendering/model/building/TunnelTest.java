289b6d21c78800aa9b383811942a458a5fd7e599
Viktor Varga <viktor.varga@sas.com>
2018-04-21 13:54:42 +0200

    added tests for prepareLightning

--- sources/codemetropolis-toolchain-rendering/test/codemetropolis/toolchain/rendering/model/building/TunnelTest.java
+++ sources/codemetropolis-toolchain-rendering/test/codemetropolis/toolchain/rendering/model/building/TunnelTest.java
@@ -2,10 +2,13 @@ package codemetropolis.toolchain.rendering.model.building;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
+import java.util.List;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -14,6 +17,7 @@ import codemetropolis.toolchain.commons.cmxml.Buildable;
 import codemetropolis.toolchain.commons.cmxml.Point;
 import codemetropolis.toolchain.rendering.control.WorldBuilder;
 import codemetropolis.toolchain.rendering.exceptions.RenderingException;
+import codemetropolis.toolchain.rendering.model.primitive.SolidBox;
 
 
 /** Test class for {@link Tunnel} for basic functions.
@@ -100,4 +104,64 @@ public class TunnelTest {
 		assertTrue( tunnel.calculateStepPosition(true).getX() == tunnel.innerBuildable.getPositionX() ||
 					tunnel.calculateStepPosition(true).getZ() == tunnel.innerBuildable.getPositionZ());
 	}
+	
+	@Test
+	public void testLampPositionY() {
+		SolidBox lampLine = tunnel.getLighting().get(0);
+		
+		int actual = lampLine.getBasePoint().getY();
+		int expected = tunnel.level - 1;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testPrepareLightingReturnsValidList() {
+		List<SolidBox> lighting = tunnel.prepareLighting();
+		
+		assertNotNull(lighting);
+		
+		int expectedSize = 2;
+		int actualSize = lighting.size();
+		assertEquals(expectedSize, actualSize);
+	}
+	
+	@Test
+	public void testPrepareLightingReturnsElementsNotNull() {
+		List<SolidBox> lighting = tunnel.prepareLighting();
+		
+		SolidBox lampLine = lighting.get(0);
+		assertNotNull(lampLine);
+		
+		SolidBox redstoneBlockLine = lighting.get(1);
+		assertNotNull(redstoneBlockLine);
+	}
+	
+	@Test
+	public void testPrepareLightningReturnsRightLampline() {
+		List<SolidBox> lighting = tunnel.prepareLighting();
+		SolidBox lampLine = lighting.get(0);
+		
+		int actualPositionY = lampLine.getBasePoint().getY();
+		int expectedPositionY = tunnel.level - 1;
+		assertEquals(expectedPositionY, actualPositionY);
+		
+		int actualNBlocks = lampLine.getNumberOfBlocks();
+		int expectedNBlocks = Math.abs((tunnel.getSize().getX()-2) * (tunnel.getSize().getY()-2) * (tunnel.getSize().getZ()-2));
+		assertEquals(expectedNBlocks, actualNBlocks);
+	}
+	
+	@Test
+	public void testPrepareLightningReturnsRightRedstoneLine() {
+		List<SolidBox> lighting = tunnel.prepareLighting();
+		SolidBox redstoneLine = lighting.get(1);
+		
+		int actualPositionY = redstoneLine.getBasePoint().getY();
+		int expectedPositionY = tunnel.level - 2;
+		assertEquals(expectedPositionY, actualPositionY);
+		
+		int actualNBlocks = redstoneLine.getNumberOfBlocks();
+		int expectedNBlocks = Math.abs((tunnel.getSize().getX()-2) * (tunnel.getSize().getY()-2) * (tunnel.getSize().getZ()-2));
+		assertEquals(expectedNBlocks, actualNBlocks);
+	}
+	
 }
