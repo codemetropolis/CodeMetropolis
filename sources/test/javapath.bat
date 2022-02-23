e0d7421a6aa2e3f4c7e7fde0154978ee65960ee7
TheRozon <zoltan.banyai01999@gmail.com>
2022-02-23 20:23:26 +0100

    pytest run outsource into a function and folder management

--- sources/test/javapath.bat
+++
@@ -1,3 +0,0 @@
-setlocal 
-    SET PATH=C:/Program Files/Java/jre1.8.0_301/bin;%PATH%
-    java.exe -jar ../distro/mapping-1.4.0.jar -i IO/inputs/sanyika.xml -m mapping_IO/inputs/sourcemeter_mapping_example.xml
--- sources/test/tester.py
+++ sources/test/tester.py
@@ -6,16 +6,16 @@ import shutil
 import sys
 import argparse
 
-parser = argparse.ArgumentParser(description='CodeMetropolis tester')
+parser = argparse.ArgumentParser(description='CodeMetropolis tester wich generate an output from selected input file and compare it with an excepted file')
 
 parser.add_argument('--pyfile', dest='pyfile', type=str,
-                    help='run a specific pytest file to test from pytests folder')
+                    help='pytest file name without extension')
 parser.add_argument('--all', dest='all', type=str,
-                    help='all')                   
+                    help='all keyword to run all pytest file from the selected folder')                   
 parser.add_argument('folder', metavar='F', type=str,
-                    help='specific name of the folder from the pytests folder (converter/mapping/placing/rendering)')
+                    help='If use --pyfile: the specific folder name in pytests folder where the selected pytest(s) exist\n ')
 parser.add_argument('input', metavar='I', type=str,
-                    help='input file name')                  
+                    help='Selected input file name from IO/input directory')                  
 
 #pelda
 # python tester.py --pyfile elementNameMatch_test converter inputFile
@@ -31,9 +31,11 @@ pytestPath = 'pytests/' + pytestFolder + "/"
 sys.path.append(os.path.join(os.path.dirname(__file__), pytestPath))
 
 #import_pytest
+#if_--pytest
 pyFileName = args.pyfile
 if (pyFileName is not None):
     pyFile = __import__(pyFileName)
+#if_--all
 if (pyFileName is None):
     from os import walk
     fileNames = next(walk(pytestPath), (None, None, []))[2]
@@ -43,19 +45,22 @@ if (pyFileName is None):
 
 jarFile = pyFile.jar
 
-#run_pytest
+#run_pytest_function
 allPytestRun = args.all
 def pytestRunner(pyFile):
     pytest.main(['-x', pytestPath + pyFile + '.py'])
+
+def allPytestRunCheck(allPytestRunFunc):
+    if (allPytestRunFunc is None):
+        pytestRunner(args.pyfile)
+    if (allPytestRunFunc is not None):
+        pytest.main(["-x", pytestPath])
 
 #run jars
 if('converter' in jarFile):
     subprocess.call(['java.exe', '-jar', '../distro/converter-1.4.0.jar', '-t' ,'sourcemeter', '-s' , '' + pyFile.input + input + ".graph"]),
     shutil.move('converterToMapping.xml', '' + pyFile.output)
-    if (allPytestRun is None):
-        pytestRunner(args.pyfile)
-    if (allPytestRun is not None):
-        pytest.main(["-x", pytestPath])  
+    allPytestRunCheck(allPytestRun)
 
 
 if('mapping' in jarFile):
@@ -67,19 +72,14 @@ if('mapping' in jarFile):
     javaPathFile.close();
     subprocess.call([r'javapath.bat'])
     shutil.move("mappingToPlacing.xml", '' + pyFile.output)
-    if (allPytestRun is None):
-        pytestRunner(args.pyfile)
-    if (allPytestRun is not None):
-        pytest.main(["-x", pytestPath])  
+    os.remove('javapath.bat')
+    allPytestRunCheck(allPytestRun)
 
 
 if('placing' in jarFile):
     subprocess.call(['java.exe', '-jar', '../distro/placing-1.4.0.jar', '-i', '' + pyFile.input])
     shutil.move("placingToRendering.xml", '' + pyFile.output)
-    if (allPytestRun is None):
-        pytestRunner(args.pyfile)
-    if (allPytestRun is not None):
-        pytest.main(["-x", pytestPath])  
+    allPytestRunCheck(allPytestRun)
 
 if('rendering' in jarFile):
     subprocess.call(['java.exe', '-jar', '../distro/rendering-1.4.0.jar', '-i', '' + pyFile.input, '-world', 'world'])
@@ -87,10 +87,8 @@ if('rendering' in jarFile):
     if (worldExist == True):
         shutil.rmtree(pyFile.output)  
     shutil.move("world", '' + pyFile.output)
-    if (allPytestRun is None):
-        pytestRunner(args.pyfile)
-    if (allPytestRun is not None):
-        pytest.main(["-x", pytestPath])  
+    allPytestRunCheck(allPytestRun)
+
 
 
 
