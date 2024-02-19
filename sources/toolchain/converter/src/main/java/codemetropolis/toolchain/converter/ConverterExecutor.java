69b62eb17b44478877e10dbe92a4528c9886f19c
baron <baron@inf.u-szeged.hu>
2024-02-19 06:19:36 +0100

    refactor(converting): Delegated the sub-processes into their own separate functions. Now The main method only calls and executes the related functions.

--- sources/toolchain/converter/src/main/java/codemetropolis/toolchain/converter/ConverterExecutor.java
+++ sources/toolchain/converter/src/main/java/codemetropolis/toolchain/converter/ConverterExecutor.java
@@ -2,8 +2,6 @@ package codemetropolis.toolchain.converter;
 
 import codemetropolis.toolchain.commons.cdf.CdfTree;
 import codemetropolis.toolchain.commons.cdf.converter.CdfConverter;
-import codemetropolis.toolchain.commons.cdf.converter.ConverterEvent;
-import codemetropolis.toolchain.commons.cdf.converter.ConverterEventListener;
 import codemetropolis.toolchain.commons.cdf.exceptions.CdfWriterException;
 import codemetropolis.toolchain.commons.exceptions.CodeMetropolisException;
 import codemetropolis.toolchain.commons.executor.AbstractExecutor;
@@ -16,40 +14,50 @@ public class ConverterExecutor extends AbstractExecutor {
 	@Override
 	public boolean execute(ExecutorArgs args) {
 		ConverterExecutorArgs converterArgs = (ConverterExecutorArgs) args;
-		
-		CdfConverter converter = ConverterLoader.load(converterArgs.getType(), converterArgs.getParams(), converterArgs.getVerboseMode());
-		converter.addConverterEventListener(new ConverterEventListener() {
-			
-			@Override
-			public void onConverterEvent(ConverterEvent event) {
-				print(event.getMessage());
-			}
-			
-		});
-		
+		CdfConverter converter = createConverter(converterArgs);
+
 		print(Resources.get("converting_to_cdf"));
-		CdfTree cdfTree = null;
-		try {
-			cdfTree = converter.createElements(converterArgs.getSource());
-		} catch (CodeMetropolisException e) {
-			printError(e, e.getMessage());
-			return false;
-		} catch (Exception e) {
-			printError(e, Resources.get("converter_error"));
+		CdfTree cdfTree = createElements(converter, converterArgs);
+		if (cdfTree == null) {
 			return false;
 		}
 		print(Resources.get("converting_to_cdf_done"));
-		
+
 		print(Resources.get("printing_cdf"));
+		if (!writeCdfToFile(cdfTree, converterArgs)) {
+			return false;
+		}
+		print(Resources.get("printing_cdf_done"));
+
+		return true;
+	}
+
+	private CdfConverter createConverter(ConverterExecutorArgs converterArgs) {
+		CdfConverter converter = ConverterLoader.load(converterArgs.getType(), converterArgs.getParams(),
+				converterArgs.getVerboseMode());
+		converter.addConverterEventListener(event -> print(event.getMessage()));
+		return converter;
+	}
+
+	private CdfTree createElements(CdfConverter converter, ConverterExecutorArgs converterArgs) {
+		try {
+			return converter.createElements(converterArgs.getSource());
+		} catch (CodeMetropolisException e) {
+			printError(e, e.getMessage());
+			return null;
+		} catch (Exception e) {
+			printError(e, Resources.get("converter_error"));
+			return null;
+		}
+	}
+
+	private boolean writeCdfToFile(CdfTree cdfTree, ConverterExecutorArgs converterArgs) {
 		try {
 			cdfTree.writeToFile(converterArgs.getOutputFile());
+			return true;
 		} catch (CdfWriterException e) {
 			printError(e, Resources.get("cdf_writer_error"));
 			return false;
 		}
-		print(Resources.get("printing_cdf_done"));
-		
-		return true;
 	}
-
 }
