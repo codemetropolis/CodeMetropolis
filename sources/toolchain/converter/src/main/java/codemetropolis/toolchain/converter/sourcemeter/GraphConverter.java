627beaf93caa9bc4dc9f63370fc6e5a90d964af1
baron <baron@inf.u-szeged.hu>
2024-02-28 07:06:34 +0100

    feat(converting): Added an extra for logVerbose and now exceptions will not be written on the default out channel but rather use the error one.

--- sources/toolchain/converter/src/main/java/codemetropolis/toolchain/converter/sourcemeter/GraphConverter.java
+++ sources/toolchain/converter/src/main/java/codemetropolis/toolchain/converter/sourcemeter/GraphConverter.java
@@ -30,74 +30,78 @@ public class GraphConverter extends CdfConverter {
 	
 	@Override
 	public CdfTree createElements(String graphPath) {
-		logVerbose("Creating CodeMetropolis CdfTree.");
+		logVerbose("Creating CodeMetropolis CdfTree.",false);
 
 		Graph graph = createEmptyGraph();
 		loadGraph(graph, graphPath);
 		Node root = findRootNode(graph);
 		CdfElement rootElement = createChildElements(root);
 
-		logVerbose("Creating CodeMetropolis CdfTree done.");
+		logVerbose("Creating CodeMetropolis CdfTree done.", false);
 		return new CdfTree(rootElement);
 	}
 
 	private Graph createEmptyGraph() {
-		logVerbose("Creating empty graph in which SourceMeter graph will load into.");
+		logVerbose("Creating empty graph in which SourceMeter graph will load into.", false);
 		Graph graph = new Graph();
-		logVerbose("Empty graph creation done.");
+		logVerbose("Empty graph creation done.", false);
 		return graph;
 	}
 
 	private void loadGraph(Graph graph, String graphPath) {
-		logVerbose("Loading in SourceMeter graph.");
+		logVerbose("Loading in SourceMeter graph.", false);
 		try {
 			graph.loadBinary(graphPath);
-			logVerbose("Loading in SourceMeter graph done.");
+			logVerbose("Loading in SourceMeter graph done.", false);
 		} catch (Exception e) {
-			logVerbose("Error loading SourceMeter graph: " + e.getMessage());
+			logVerbose("Error loading SourceMeter graph: " + e.getMessage(), true);
 		}
 	}
 
 	private Node findRootNode(Graph graph) {
-		logVerbose("Finding root node of SourceMeter graph.");
+		logVerbose("Finding root node of SourceMeter graph.", false);
 		try {
 			Node root = graph.findNode(ROOT_NODE_ID);
-			logVerbose("Finding root node of SourceMeter graph done.");
+			logVerbose("Finding root node of SourceMeter graph done.",false);
 			return root;
-		} catch (Exception e) {
-			logVerbose("Error loading SourceMeter graph: " + e.getMessage());
+		} catch (NullPointerException e) {
+			logVerbose("Error loading SourceMeter graph: " + e.getMessage(), true);
 			return null;
 		}
 	}
 
 	private CdfElement createChildElements(Node root) {
-		logVerbose("Creating child elements recursively.");
+		logVerbose("Creating child elements recursively.", false);
 		try {
 			CdfElement rootElement = createElementsRecursively(root);
-			logVerbose("Creating child elements recursively done.");
+			logVerbose("Creating child elements recursively done.", false);
 			return rootElement;
-		} catch (Exception e) {
-			logVerbose("Error creating child elements recursively: " + e.getMessage());
+		} catch (NullPointerException e) {
+			logVerbose("Error creating child elements recursively: " + e.getMessage(), true);
 			return null;
 		}
 	}
 
-	private void logVerbose(String message) {
+	private void logVerbose(String message, boolean isError) {
 		if (super.getVerboseMode()) {
 			String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new java.util.Date());
-			System.out.println("[" + timestamp + "]:" + message);
+			if (isError) {
+				System.err.println("[" + timestamp + "]:" + message);
+			} else {
+				System.out.println("[" + timestamp + "]:" + message);
+			}
 		}
 	}
 	
 	private CdfElement createElementsRecursively(Node root) {
 		String name = ((AttributeString)root.findAttributeByName("Name").next()).getValue();
 		String type = root.getType().getType();
-		logVerbose("Creating CodeMetropolis CdfElement: " + name + " of type: " + type);
+		logVerbose("Creating CodeMetropolis CdfElement: " + name + " of type: " + type, false);
 		CdfElement element = new CdfElement(name, type);
-		logVerbose("Creating CodeMetropolis CdfElement: " + name + " of type: " + type + " done.");
-		logVerbose("Setting source id to: " + root.getUID());
+		logVerbose("Creating CodeMetropolis CdfElement: " + name + " of type: " + type + " done.", false);
+		logVerbose("Setting source id to: " + root.getUID(), false);
 		element.setSourceId(root.getUID());
-        logVerbose("Setting source id to: " + root.getUID() + " done.");
+        logVerbose("Setting source id to: " + root.getUID() + " done.", false);
 		addProperties(root, element);
 		for(Node child : getChildNodes(root)) {
 			element.addChildElement(createElementsRecursively(child));
@@ -106,7 +110,7 @@ public class GraphConverter extends CdfConverter {
 	}
 
 	private Node[] getChildNodes(Node node) {
-		logVerbose("Getting child nodes of: " + node.getUID());
+		logVerbose("Getting child nodes of: " + node.getUID(), false);
 		List<Node> childList = new ArrayList<Node>();
 		EdgeIterator it = node.findOutEdges(new EdgeType("LogicalTree", eDirectionType.edtDirectional));
 		while(it.hasNext()) {
@@ -114,12 +118,12 @@ public class GraphConverter extends CdfConverter {
 			if(!node.getUID().equals(childNode.getUID())) 
 				childList.add(childNode);
 		}
-		logVerbose("Getting child nodes of: " + node.getUID() + " done.");
+		logVerbose("Getting child nodes of: " + node.getUID() + " done.", false);
 		return childList.toArray(new Node[childList.size()]);
 	}
 	
 	private void addProperties(Node node, CdfElement element) {
-		logVerbose("Adding properties to CodeMetropolis CdfElement: " + element.getName());
+		logVerbose("Adding properties to CodeMetropolis CdfElement: " + element.getName(), false);
 		AttributeIterator attributeIterator = node.getAttributes();
 		while(attributeIterator.hasNext()) {
 			Object value;
@@ -130,25 +134,25 @@ public class GraphConverter extends CdfConverter {
 					value = ((AttributeString)a).getValue();
 					type = CdfProperty.Type.STRING;
 					logVerbose("Adding property: " + a.getName() + " with value: " + value + " and type: " + type + " " +
-							"to CodeMetropolis CdfElement: " + element.getName() + " done.");
+							"to CodeMetropolis CdfElement: " + element.getName() + " done.", false);
 					break;
 				case atInt:
 					value = ((AttributeInt)a).getValue();
 					type = CdfProperty.Type.INT;
 					logVerbose("Adding property: " + a.getName() + " with value: " + value + " and type: " + type + " " +
-							"to CodeMetropolis CdfElement: " + element.getName() + " done.");
+							"to CodeMetropolis CdfElement: " + element.getName() + " done.", false);
 					break;
 				case atFloat:
 					value = ((AttributeFloat)a).getValue();
 					type = CdfProperty.Type.FLOAT;
 					logVerbose("Adding property: " + a.getName() + " with value: " + value + " and type: " + type + " " +
-							"to CodeMetropolis CdfElement: " + element.getName() + " done.");
+							"to CodeMetropolis CdfElement: " + element.getName() + " done.", false);
 					break;
 				default:
 					continue;
 			}
 		    element.addProperty(a.getName(), String.valueOf(value), type);
-			logVerbose("Adding properties to CodeMetropolis CdfElement: " + element.getName() + " done.");
+			logVerbose("Adding properties to CodeMetropolis CdfElement: " + element.getName() + " done.", false);
 		}
 	}
 }
