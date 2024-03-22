7aaf68a393ef931b4c5b5a97b56cef006e40f7a1
Tóth Bojnik Tibor <tothbojniktibor81@gmail.com>
2024-03-22 13:16:28 +0100

    refactor: The files of BlockModifier have been relocated under codemetropolis.toolchain.commons to be directly integrated into CodeMetropolis.
    BREAKING CHANGE: Paths and errors have not been resolved yet, so the project is not functioning properly.

---
+++ sources/commons/src/main/java/codemetropolis/toolchain/commons/ext/FileHandler.java
@@ -0,0 +1,55 @@
+package codemetropolis.blockmodifier.ext;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class FileHandler {
+
+	public static void copy(String srcPath, String destPath, String... ignore) {
+		File sourceLocation = new File(srcPath);
+		File targetLocation = new File(destPath);
+		copy(sourceLocation, targetLocation, ignore);
+	}
+	
+	public static void copy(File sourceLocation, File targetLocation, String... ignore) {
+		for(String s : ignore)
+			if(sourceLocation.getName().equals(s)) return;
+		try {
+			if (sourceLocation.isDirectory()) {
+			    copyDirectory(sourceLocation, targetLocation, ignore);
+			} else {
+			    copyFile(sourceLocation, targetLocation);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void copyDirectory(File source, File target, String... ignore) throws IOException {
+	    if (!target.exists()) {
+	        target.mkdirs();
+	    }
+
+	    for (String f : source.list()) {
+	        copy(new File(source, f), new File(target, f), ignore);
+	    }
+	}
+
+	private static void copyFile(File source, File target) throws IOException {
+		try (
+	            InputStream in = new FileInputStream(source);
+	            OutputStream out = new FileOutputStream(target)
+	    ) {
+	        byte[] buf = new byte[1024];
+	        int length;
+	        while ((length = in.read(buf)) > 0) {
+	            out.write(buf, 0, length);
+	        }
+	    }
+	}
+	
+}
