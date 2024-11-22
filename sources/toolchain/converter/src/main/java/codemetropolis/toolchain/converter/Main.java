2b615d4000e7b06354861fa46a782d83d23ca453
Búcsú Áron <baron@inf.u-szeged.hu>
2024-11-22 12:30:59 +0100

    refactor: update converter main class to align with new help message and to be more similar in structure to similar modules like mapping

--- sources/toolchain/converter/src/main/java/codemetropolis/toolchain/converter/Main.java
+++ sources/toolchain/converter/src/main/java/codemetropolis/toolchain/converter/Main.java
@@ -1,8 +1,5 @@
 package codemetropolis.toolchain.converter;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 
@@ -11,96 +8,113 @@ import codemetropolis.toolchain.commons.util.Resources;
 import codemetropolis.toolchain.commons.util.Settings;
 import codemetropolis.toolchain.converter.control.ConverterType;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
-	/**
-	 * If there is no parameter given, the system will print out the 'without_param' string from the resources,
-	 * which tells the user that no parameter was given,
-	 * and also print the 'converter_usage' string.
-	 */
-	public static boolean checkWithoutParam(CommandLineOptions options) {
-		if(options.getType() == null || options.getSource() == null ) {
-
-			System.err.println(Resources.get("without_param"));
-			System.err.println(Resources.get("converter_usage"));
-
-			return true;
-		}
-		return false;
-	}
+	private static final String CONVERTER_LOG_FILE = "converter_log_file";
+	private static final String CONVERTER_HELP_MESSAGE_WHEN_NO_ARGS = "converter_usage_and_help";
+	private static final String COMMAND_LINE_ERROR = "command_line_error";
+	private static final String CONVERTER_USAGE = "converter_usage_and_help";
+	private static final String CONVERTER_INTRODUCTION = "converter_introduction";
+	private static final String CONVERTER_PREFIX = "converter_prefix";
+	private static final String ERROR_PREFIX = "error_prefix";
 
 	public static void main(String[] args) {
-		
-		FileLogger.load(Settings.get("converter_log_file"));
-		
+		FileLogger.load(Settings.get(CONVERTER_LOG_FILE));
+
 		CommandLineOptions options = new CommandLineOptions();
-	    CmdLineParser parser = new CmdLineParser(options);
+		CmdLineParser parser = new CmdLineParser(options);
 
-	    try {
-	        parser.parseArgument(args);
+		try {
+			parser.parseArgument(args);
 
-			if(checkWithoutParam(options)) {
+			if (args.length == 0) {
+				printHelpMessage();
 				return;
 			}
+		} catch (CmdLineException | IllegalArgumentException e) {
+			logAndPrintError(e);
+			return;
+		}
 
-	        if(options.getType() == null || options.getSource() == null ){
-				throw new IllegalArgumentException();
-			}
-	    } catch (CmdLineException | IllegalArgumentException e) {
-	    	String message = Resources.get("command_line_error");
-	    	FileLogger.logError(message, e);
-	    	System.err.println(message);
-	    	System.err.println(Resources.get("converter_usage"));
-	    	return;
-	    }
+		if (options.showHelp()) {
+			printUsage();
+			return;
+		}
 
-		if(options.getVerboseMode() && !options.showHelp()) {
-	    	System.out.println(Resources.get("verbose_mode_enabled"));
-	    }
-
-	    ConverterType converterType;
-	    try {
-	    	converterType = ConverterType.valueOf(options.getType().toUpperCase());
-	    } catch(IllegalArgumentException e) {
-	    	String message = String.format("%s%s", Resources.get("error_prefix"), Resources.get("invalid_converter_type"));
-	    	System.err.println(message);
-	    	FileLogger.logError(message, e);
-	    	return;
-	    }
-	    
-	    Map<String, String> params = new HashMap<>();
-	    if(options.getParams() != null) {
-	    	try {
-	 		    String[] paramsArray = options.getParams();
-	 		    for(String str : paramsArray) {
-	 		    	String[] strParts = str.split("=");
-	 		    	params.put(strParts[0], strParts[1]);
-	 		    }
-	 	    } catch(Exception e) {
-	 	    	String message = Resources.get("invalid_params");
-	 	    	System.err.println(message);
-	 	    	FileLogger.logError(message, e);
-	 	    }
-	    }
-	    
-	    if(options.showHelp()) {
-	    	System.out.println(Resources.get("converter_introduction"));
-	    	System.out.println(Resources.get("converter_usage"));
-	    	return;
-	    }
-			
-	    ConverterExecutor executor = new ConverterExecutor();
-	    executor.setPrefix(Resources.get("converter_prefix"));
-	    executor.setErrorPrefix(Resources.get("error_prefix"));
-	    executor.execute(
-	    		new ConverterExecutorArgs(
-	    			converterType,
-		    		options.getSource(),
-		    		options.getOutputFile(),
-		    		params,
-					options.getVerboseMode()
-	    		));	
-		
+		try {
+			executeConversion(options);
+		} catch (IllegalArgumentException e) {
+			logAndPrintError(e);
+		}
 	}
-	
+
+	private static void printHelpMessage() {
+		System.out.println(Resources.get(CONVERTER_HELP_MESSAGE_WHEN_NO_ARGS));
+	}
+
+	private static void logAndPrintError(Exception e) {
+		String message = Resources.get(COMMAND_LINE_ERROR);
+		FileLogger.logError(message, e);
+		System.err.println(message);
+		System.err.println(Resources.get(CONVERTER_USAGE));
+	}
+
+	private static void printUsage() {
+		System.out.println(Resources.get(CONVERTER_INTRODUCTION));
+		System.out.println(Resources.get(CONVERTER_USAGE));
+	}
+
+	private static void executeConversion(CommandLineOptions options) {
+		validateOptions(options);
+
+		ConverterType converterType;
+		try {
+			converterType = ConverterType.valueOf(options.getType().toUpperCase());
+		} catch (IllegalArgumentException e) {
+			String message = String.format("%s%s", Resources.get("error_prefix"), Resources.get("invalid_converter_type"));
+			System.err.println(message);
+			FileLogger.logError(message, e);
+			return;
+		}
+
+		Map<String, String> params = new HashMap<>();
+		if (options.getParams() != null) {
+			try {
+				String[] paramsArray = options.getParams();
+				for (String str : paramsArray) {
+					String[] strParts = str.split("=");
+					params.put(strParts[0], strParts[1]);
+				}
+			} catch (Exception e) {
+				String message = Resources.get("invalid_params");
+				System.err.println(message);
+				FileLogger.logError(message, e);
+			}
+		}
+
+		ConverterExecutor executor = new ConverterExecutor();
+		executor.setPrefix(Resources.get(CONVERTER_PREFIX));
+		executor.setErrorPrefix(Resources.get(ERROR_PREFIX));
+		executor.execute(
+				new ConverterExecutorArgs(
+						converterType,
+						options.getSource(),
+						options.getOutputFile(),
+						params,
+						options.getVerboseMode()
+				));
+	}
+
+	private static void validateOptions(CommandLineOptions options) {
+		if (options.getType() == null || options.getType().isEmpty()) {
+			throw new IllegalArgumentException();
+		}
+
+		if (options.getSource() == null || options.getSource().isEmpty()) {
+			throw new IllegalArgumentException();
+		}
+	}
 }
